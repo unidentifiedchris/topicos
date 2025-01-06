d1afff9a1b95edbc24c5a75e3dc6516649308526
haucab <57472571+haucab@users.noreply.github.com>
2025-01-06 21:23:00 +0000

    [REF] agregar tests para hacer el resto de proyecto bajo TDD

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express"),
+    bodyParser = require("body-parser"),
+    swaggerJsdoc = require("swagger-jsdoc"),
+    swaggerUi = require("swagger-ui-express");
+
+const app = express();
+const options = {
+    definition: {
+        openapi: "3.1.0",
+        info: {
+            title: "ApiChistes",
+            version: "0.1.0",
+            description: "Proyecto TEP",
+        },
+        servers: [
+            {
+                url: "http://localhost:3000",
+            },
+        ],
+    },
+    apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs, { explorer: true })
+);
+app.use(bodyParser.json());
+app.use(require("./routes/jokesroutes"));
+
+module.exports = app;
